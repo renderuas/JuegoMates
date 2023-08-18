df097f91098eb3787e41cfc2076b2e1067e2f7f3
renderuas <renderuas@gmail.com>
2023-08-18 10:28:23 +0000

    actiañlizacion de librerias

--- math-kids-frontend/src/App.js
+++ math-kids-frontend/src/App.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+import unicornioAnimation from './images/unicornio1.json'; // Importa la animación
 import './App.css'; // Asumiendo que tienes un archivo de estilos llamado App.css
 import SumaResta from './components/SumaResta'; // Importando el componente que creamos
 
 function App() {
   // Este estado manejará el juego que el usuario ha seleccionado
   const [juegoSeleccionado, setJuegoSeleccionado] = useState(null);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: unicornioAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   return (
     <div className="app">
@@ -25,7 +36,16 @@ function App() {
       </section>
 
       <section className="game-content">
-        {juegoSeleccionado === 'sumaResta' && <SumaResta />}
+        {juegoSeleccionado === 'sumaResta' && (
+          <div className="game-container">
+            <Lottie 
+              options={defaultOptions}
+              height={300} // Puedes ajustar este valor
+              width={300} // Puedes ajustar este valor
+            />
+            <SumaResta />
+          </div>
+        )}
         {/* Si añades más juegos, solo necesitas agregar condiciones similares aquí */}
       </section>
 
